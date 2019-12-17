0868761507bed2afd079d1f4f19150dd112a3dd8
ChihYang41 <Jigsaw0401@gmail.com>
2019-12-18 00:00:58 +0800

    Updates

---
+++ precache-manifest.17a3021a7ed25dd34b2a2f533f15d977.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e3b0a8e573595286feb9d0d64186ae",
+    "url": "/React-Github-API-Viewer/index.html"
+  },
+  {
+    "revision": "4e9be5639f61f27d2515",
+    "url": "/React-Github-API-Viewer/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "6e08236827737698d4e1",
+    "url": "/React-Github-API-Viewer/static/css/main.5ddfb7c6.chunk.css"
+  },
+  {
+    "revision": "4e9be5639f61f27d2515",
+    "url": "/React-Github-API-Viewer/static/js/2.2fc974b5.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/React-Github-API-Viewer/static/js/2.2fc974b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e08236827737698d4e1",
+    "url": "/React-Github-API-Viewer/static/js/main.c37dc190.chunk.js"
+  },
+  {
+    "revision": "33c18d7c7867abe30ce2",
+    "url": "/React-Github-API-Viewer/static/js/runtime-main.851b3582.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Github-API-Viewer/static/media/logo.5d5d9eef.svg"
+  }
+]);
